cdc5aa655d9b35c23783cf95b7e97c82d1785938
Chanaka Karunarathne <kmchmk@gmail.com>
2021-06-27 19:01:05 +0530

    Update README file

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # SLT-Data-Usage
-This script can get your SLT data usage and display it in the system tray.
+This script can get your SLT data usage and display it in the system tray. This is mostly useful if you are using a unlimited package with daily limit.
+
+Refresh interval is 120 secs, but it can easily be changed in the code.
+
+You have to get some configs using Inspect Element option in the browser.
+https://internetvas.slt.lk/
+
+Initial setup:
+```
+pip install pystray
+pip install requests
+```
--- system_tray.py
+++ system_tray.py
@@ -14,7 +14,7 @@ stop_itr_duration = 3 #seconds
 def refresh(icon):
     image = get_empty_image()
     draw = ImageDraw.Draw(image)
-    draw.text(TOP_LEFT, data_usage.get_data_usage(), font = font_type, fill = 'yellow')
+    draw.text(TOP_LEFT, data_usage.get_data_usage(), font = font_type, fill = 'yellow') # ToDo - I used yellow here because of the System wide Dark theme
     icon.icon = image
 
 def update_forever(icon): # Exit method should be faster than refresh interval. So we have 2 iterations here.
@@ -34,7 +34,7 @@ def exit_method(icon):
     icon.stop()
 
 menu = (pystray.MenuItem('Refresh', refresh), pystray.MenuItem('Exit', exit_method))
-icon = pystray.Icon("icon1", get_empty_image(), "Title", menu)
+icon = pystray.Icon("icon1", get_empty_image(), "SLT Daily Usage (GB)", menu)
 icon.visible = True
 icon.run(setup=update_forever)
 icon.stop()
