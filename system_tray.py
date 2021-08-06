8dd284420164e12767a6674cb2f2a8648822e9e3
Chanaka Karunarathne <kmchmk@gmail.com>
2021-08-06 21:59:25 +0530

    Improve thread structure

--- system_tray.py
+++ system_tray.py
@@ -1,19 +1,15 @@
-from warnings import filterwarnings
 import pystray
 from PIL import Image, ImageDraw, ImageFont
 import time
-import random
-import threading
 import data_usage
 import input_ui
 
 size = 100
-font_type = ImageFont.truetype("arial.ttf", size//2)
+font_type = ImageFont.truetype("tahoma.ttf", size//2)
 TOP_LEFT = (0, 0)
 def get_empty_image(): return Image.new('RGBA', (size, size//2))  # Empty image
 
 
-finished = False
 refresh_itr_duration = 120  # seconds
 
 
@@ -27,25 +23,19 @@ def refresh(icon):
 
 
 def update_forever(icon):
-    while not finished:
+    icon.visible = True
+    while icon.visible:
         refresh(icon)
         time.sleep(refresh_itr_duration)
-    icon.stop()
 
 
 def exit_method(icon):
-    global finished
-    finished = True
     icon.visible = False
+    icon.stop()
 
 
 menu = (pystray.MenuItem('Refresh', refresh), pystray.MenuItem(
     'Change account', input_ui.change_account), pystray.MenuItem('Exit', exit_method))
 icon = pystray.Icon("icon1", get_empty_image(), "SLT Daily Usage (GB)", menu)
 
-update_thread = threading.Thread(target=update_forever, args=(icon,))
-update_thread.start()
-
-icon.run()
-
-update_thread.join()
+icon.run(update_forever)
